2386f3ea6934618c7623c918ba5483377eb56d54
Otchy <id+github@otchy.net>
2021-07-25 00:02:00 +0900

    Add search pane

---
+++ src/content/Pane.tsx
@@ -0,0 +1,53 @@
+import React, { FC, useState } from "react";
+import styled from "styled-components";
+
+const contentHeight = '320px';
+
+interface ContainerProps {
+    opened: boolean;
+};
+
+const Container = styled.div<ContainerProps>`
+    display: flex;
+    flex-direction: column;
+    margin-top: ${props => props.opened ? '0' : `-${contentHeight}`};
+    transition: margin 200ms ease;
+`;
+
+const Content = styled.iframe`
+    border: none;
+    box-sizing: border-box;
+    width: 100vw;
+    height: ${contentHeight};
+    background-color: #999;
+    border-style: solid;
+    border-width: 0 2px 2px 2px;
+    border-color: #fff;
+`;
+
+const Tab = styled.span`
+    display: inline-block;
+    margin-top: -2px;
+    background-color: #999;
+    background-image: url(${chrome.extension.getURL('img/icon_48.png')});
+    background-size: 24px;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 32px;
+    height: 32px;
+    border-style: solid;
+    border-width: 0px 2px 2px 2px;
+    border-color: #fff;
+    border-radius: 0 0 4px 4px;
+    cursor: pointer;
+`;
+
+const Pane: FC = () => {
+    const [opened, setOpened] = useState(false);
+    return <Container opened={opened}>
+        <Content />
+        <Tab onClick={() => setOpened(!opened)} />
+    </Container>
+};
+
+export default Pane;
--- src/content/init.tsx
+++ src/content/init.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDom from 'react-dom';
 import { log } from "../common";
+import Pane from "./Pane";
 
 export const init = (site: SiteConfig): void => {
     log(`Initializing ${site.title}`);
     const tsrRoot = document.createElement('span');
+    tsrRoot.style.position = 'fixed';
+    tsrRoot.style.left = '0';
+    tsrRoot.style.top = '0';
+    tsrRoot.style.zIndex = '9999';
     document.body.appendChild(tsrRoot);
-    const text = <div>TESTTEST</div>;
-    ReactDom.render(text, tsrRoot);
+    ReactDom.render(<Pane />, tsrRoot);
 };
