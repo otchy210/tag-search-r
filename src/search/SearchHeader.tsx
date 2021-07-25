c038558d807d2a96ec347d606559882dc514b25a
Otchy <id+github@otchy.net>
2021-07-25 13:12:33 +0900

    Design search header

--- src/search/SearchHeader.tsx
+++ src/search/SearchHeader.tsx
@@ -2,6 +2,20 @@ import React, { FC } from "react";
 import styled from "styled-components";
 
 const Header = styled.header`
+    display: flex;
+`;
+
+const Title = styled.h1`
+    margin: 0;
+    padding: 0.25em 0.5em;
+    font-size: 1.25em;
+`;
+
+const Selector = styled.select`
+`;
+
+const QueryBox = styled.input`
+    flex-grow: 1;
 `;
 
 interface Props {
@@ -10,7 +24,13 @@ interface Props {
 
 const SearchHeader: FC<Props> = ({site}) => {
     return <Header>
-        {site.title}
+        <Title>{site.title}</Title>
+        <Selector>
+            {site.searches.map(search => {
+                return <option>{search.title}</option>
+            })}
+        </Selector>
+        <QueryBox />
     </Header>
 };
 
